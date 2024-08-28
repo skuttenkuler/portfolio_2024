e4e8222635cc0739c83d27d29af92e3b89f6d228
skuttenkuler <sam.kuttenk@gmail.com>
2024-08-28 09:45:04 -0500

    more styling contact form working and updated contact section

--- src/components/Contact/Form.jsx
+++ src/components/Contact/Form.jsx
@@ -1,9 +1,9 @@
 
-import React, {useState} from 'react'
+import React, {useState,useEffect} from 'react'
 import axios from 'axios'
 
 export const ContactForm = () => {
-   // const URL = process.env.NEXT_PUBLIC_SES_URL
+    const [success,setSuccess] = useState(false);
     const [input, setInputs] = useState({   name: '', 
                                             email: '', 
                                             message: ''
@@ -12,41 +12,54 @@ export const ContactForm = () => {
     const handleChange = (e) => {
         setInputs({...input, [e.target.name]: e.target.value})
     };
+    useEffect(()=> {
+        if(success){
+            setTimeout(()=> {
+                setSuccess(false)
+            },3000)
+        }
+        return () => clearTimeout()
+    },[success])
 
     async function handleSubmit(e) {
         e.preventDefault()
         
-        // const data = JSON.stringify(input)
-        // await axios.post(URL, data, {
-        //     dataType: "json",
-        //     crossDomain: "true",
-        //     contentType: "application/json; charset=utf-8",
-        //     data: data,
-        // })
-        // .then(function(response) {
-        //     router.push("/success")
-        //     console.log(response.statusText, response.status,response.data);
-            
-        //   })
-        //   .catch(function(error) {
-        //     console.log(error);
-        // })
+        const data = JSON.stringify(input)
+        await axios.post(import.meta.env.VITE_SES_ENDPOINT, data, {
+            dataType: "json",
+            crossDomain: "true",
+            contentType: "application/json; charset=utf-8",
+            data: data,
+        })
+        .then(function(response) {
+            console.log(response.statusText, response.status,response.data);
+            setSuccess(true)
+          })
+          .catch(function(error) {
+            console.log(error);
+        })
     }
     
         return(
-            <form  onSubmit={handleSubmit} name="contact" href="/success" method="POST">
-                <p>
+            <>
+            {!success ?
+                <form onSubmit={handleSubmit} name="contact" href="/success" method="POST">
+                <div>
                     <input  type="text" id="name" name="name" value={input.name} placeholder="Name.." onChange={handleChange} required/>
-                </p>
-                <p>
+                </div>
+                <div>
                     <input  type="text" id="email" name="email" value={input.email} placeholder="Email.." onChange={handleChange} required/>
-                </p>
-                <p>
+                </div>
+                <div>
                     <textarea  id="message" name="message" value={input.message} placeholder="What do you want to build?" onChange={handleChange} required></textarea>
-                </p>
-                <p>
+                </div>
+                <div>
                     <button>Send</button>
-                </p>
+                </div>
             </form>
+            :
+            <h1>success</h1>
+            }
+            </>
         )
     }
